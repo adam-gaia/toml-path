a0256c345a3e28148d121cfbaf7c96cc148d1985
Adam Gaia <git@agaia.dev>
2024-09-20 18:18:27 -0600

    feat: add devenv for devshell

--- devshell.nix
+++ devshell.nix
@@ -16,8 +16,38 @@
   treefmt-module = treefmt-module-builder pkgs (import ./treefmt.nix);
   treefmt-bin = treefmt-module.wrapper;
   treefmt-programs = lib.attrValues treefmt-module.programs;
+
+  # Grab cargo, clippy, rustfmt, etc from crane's devShell to put in our own
+  craneToolchain = (craneLib.devShell {} ).nativeBuildInputs;
 in
-  craneLib.devShell {
+
+  inputs.devenv.lib.mkShell {
+    inherit inputs pkgs;
+    modules = [
+      {        
+        packages = with pkgs; [
+          rust-analyzer
+          cargo-limit
+          cargo-nextest
+          just
+          bacon
+          cargo-dist
+          oranda
+          treefmt-bin
+          typos
+          perSystem.cargo-nextest-xdg.default
+        ] ++ craneToolchain ++ treefmt-programs;
+
+        enterShell = ''
+          just --list
+        '';
+
+      }
+    ];
+  }
+
+
+  #craneLib.devShell {
     # Inherit inputs from checks.
     #checks = self.checks.${system}; TODO
 
@@ -25,17 +55,5 @@ in
     # MY_CUSTOM_DEVELOPMENT_VAR = "something else";
 
     # Extra inputs can be added here; cargo and rustc are provided by default.
-    packages = with pkgs;
-      [
-        cargo-limit
-        cargo-nextest
-        just
-        bacon
-        cargo-dist
-        oranda
-        treefmt-bin
-        typos
-        perSystem.cargo-nextest-xdg.default
-      ]
-      ++ treefmt-programs;
-  }
+
+  #}
--- flake.nix
+++ flake.nix
@@ -9,6 +9,11 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
+    devenv = {
+      url = "github:cachix/devenv";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     crane.url = "github:ipetkov/crane";
 
     fenix = {
