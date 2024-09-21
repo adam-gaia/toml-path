5a02779ae604a168a14bef7518981ea4d1e73d76
Adam Gaia <git@agaia.dev>
2024-09-20 18:38:19 -0600

    fix: fix nix build by adding 'README.md' into crane's build context

--- lib/default.nix
+++ lib/default.nix
@@ -6,7 +6,19 @@
   mkCrateBuilder = pkgs: let
     inherit (pkgs) lib;
     craneLib = inputs.crane.mkLib pkgs;
-    src = craneLib.cleanCargoSource ../.;
+
+    src = let
+        # This crate includes the README.md in the api docs. We need to include the readme.md
+        # See https://crane.dev/source-filtering.html
+        markdownFilter = path: _type: builtins.match ".*md$" path != null;
+        markdownOrCargo = path: type: (markdownFilter path type) || (craneLib.filterCargoSources path type);
+    in lib.cleanSourceWith {
+      src = ../.; # The original, unfiltered source
+      filter = markdownOrCargo;
+      name = "source"; # Be reproducible, regardless of the directory name
+    };
+
+    #src = craneLib.cleanCargoSource ../.;
 
     # Common arguments can be set here to avoid repeating them later
     commonArgs = {
