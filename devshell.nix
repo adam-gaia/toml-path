398af6790231477ddff58fae191b1ada42f211f5
Adam Gaia <git@agaia.dev>
2024-09-20 19:35:02 -0600

    feat: add pre-commit hook to run treefmt

--- devshell.nix
+++ devshell.nix
@@ -19,47 +19,50 @@
   treefmt-programs = lib.attrValues treefmt-module.programs;
 
   # Grab cargo, clippy, rustfmt, etc from crane's devShell to put in our own
-  craneToolchain = (craneLib.devShell {} ).nativeBuildInputs;
+  craneToolchain = (craneLib.devShell {}).nativeBuildInputs;
 in
-
   inputs.devenv.lib.mkShell {
     inherit inputs pkgs;
     modules = [
-      {        
-        packages = with pkgs; [
-          rust-analyzer
-          cargo-limit
-          cargo-nextest
-          cargo-dist
-          just
-          bacon
-          oranda
-          typos
-          perSystem.cargo-nextest-xdg.default
-        ]
+      {
+        packages = with pkgs;
+          [
+            rust-analyzer
+            cargo-limit
+            cargo-nextest
+            cargo-dist
+            just
+            bacon
+            oranda
+            typos
+            perSystem.cargo-nextest-xdg.default
+          ]
           # Include the extra packages we use to build our crate
           ++ commonArgs.buildInputs
           # Include crane's toolchain (cargo, clippy, rustfmt, etc)
           ++ craneToolchain
           # Include treefmt and formatters
-          ++ treefmt-programs ++ [ treefmt-bin] ;
+          ++ treefmt-programs
+          ++ [treefmt-bin];
 
         enterShell = ''
           just --list
         '';
 
+        pre-commit.hooks = {
+          treefmt = {
+            enable = true;
+            package = treefmt-bin;
+          };
+        };
       }
     ];
   }
+#craneLib.devShell {
+# Inherit inputs from checks.
+#checks = self.checks.${system}; TODO
+# Additional dev-shell environment variables can be set directly
+# MY_CUSTOM_DEVELOPMENT_VAR = "something else";
+# Extra inputs can be added here; cargo and rustc are provided by default.
+#}
 
-
-  #craneLib.devShell {
-    # Inherit inputs from checks.
-    #checks = self.checks.${system}; TODO
-
-    # Additional dev-shell environment variables can be set directly
-    # MY_CUSTOM_DEVELOPMENT_VAR = "something else";
-
-    # Extra inputs can be added here; cargo and rustc are provided by default.
-
-  #}
